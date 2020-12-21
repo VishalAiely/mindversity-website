3be066bdf362fa6489f916727e9ebd4149e90af9
VishalAiely <vaiely@vols.utk.edu>
2020-12-20 20:19:52 -0600

    fixed build

--- pages/api/resource/addResource.ts
+++ pages/api/resource/addResource.ts
@@ -6,7 +6,7 @@ import { Resource } from "utils/types";
 export default auth("admin", async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const resourceData = req.body as Resource;
-        await addResource(resource);
+        await addResource(resourceData);
         res.status(200).json({ 
             success: true, 
             payload: {}
